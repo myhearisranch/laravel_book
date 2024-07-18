f80f0dc9695b85c945cd3ef6bbf5e0607c174cad
EC2 Default User <ec2-user@ip-172-31-35-52.ap-southeast-2.compute.internal>
2024-07-18 01:11:33 +0000

    #6 本の追加登録処理

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\Validator; 
 
 Route::get('/', function () {
     return view('books');
@@ -15,6 +17,37 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::post('/books', function(Request $request){
+   //バリデーション
+   //Validator::makeメソッドを使用してバリデーションルールを定義
+   //$request->all(): 送信されたすべてのデータを配列として取得
+   $validator = Validator::make($request->all(), [
+       
+        //カラム名  => ルール
+        'item_name' => 'required|max:255',   
+    ]);
+    
+    //バリデーションエラー(バリデーションが失敗した場合の処理)
+    if($validator -> fails()) {
+        return redirect('/')
+            //withInput(): ユーザーが入力したデータをセッションにフラッシュし、次のリクエストで再表示できるようにする
+            -> withInput()
+            //withErrors($validator): バリデーションエラーメッセージをセッションにフラッシュする。。
+            //これにより、ビューでエラーメッセージを表示する。
+            //バリデーションエラー内容を$error変数に渡している
+            -> withErrors($validator);
+    }
+    
+    //本の登録処理
+    $books = new Book;
+    $books -> item_name = $request->item_name;
+    $books -> item_number = '1';
+    $books -> item_amount = '1000';
+    $books -> published = '2017-03-07 00:00:00';
+    $books -> save();
+    return redirect('/');
 });
 
 
